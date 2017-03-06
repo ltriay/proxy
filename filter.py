45ef75962d46f3bb286f7b3b8e0755465064aed2
Lionel Triay <github@lioneltriay.net>
2017-03-06 14:14:05 +0100

    Testing request function

--- filter.py
+++ filter.py
@@ -4,31 +4,30 @@ without sending any data to the remote server.
 """
 from mitmproxy import http
 from mitmproxy import flowfilter
-from mitmproxy import ctx		# log facility
+from mitmproxy import ctx             # log facility
 
-
-def request(flow):
-    # pretty_url takes the "Host" header of the request into account, which
-    # is useful in transparent mode where we usually only have the IP otherwise.
-
-    if flow.request.pretty_url == "http://latribune.fr/":
-        flow.response = http.HTTPResponse.make(
-            200,  # (optional) status code
-            b"Hello World",  # (optional) content
-            {"Content-Type": "text/html"}  # (optional) headers
-)
 
 class Filter:
-    def __init__(self, spec):
-        self.filter = flowfilter.parse(spec)
+	def __init__(self, spec):
+		self.filter = flowfilter.parse(spec)
 
-    def response(self, flow):
-        if flowfilter.match(self.filter, flow):
-            print("Flow matches filter:")
-            print(flow)
+	def response(self, flow):
+		if flowfilter.match(self.filter, flow):
+			print("Flow matches filter:")
+			print(flow)
 
+	def request(flow):
+		# pretty_url takes the "Host" header of the request into account, which
+		# is useful in transparent mode where we usually only have the IP otherwise.
+
+		if flow.request.pretty_url == "http://latribune.fr/":
+			flow.response = http.HTTPResponse.make(
+		    		200,  # (optional) status code
+		    		b"Hello World",  # (optional) content
+		    		{"Content-Type": "text/html"}  # (optional) headers
+			)
 
 def start():
 	ctx.log.info("This is some informative text.")
 	ctx.log.error("This is an error.")
-	return Filter("~u zapataz.net")
+	return Filter("~u lioneltriay.net")
